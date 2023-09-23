89f38fbed871dc85fcfe34c149e617919b7d1b4b
Sumit639 <sumitkkumar639@gmail.com>
2023-09-23 12:35:41 +0530

    update in menu page of more page

---
+++ app/Cakepg/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Data from '@/Data/data.json'
+
+const Cake = () => {
+  return (
+    <>
+    
+    <div>fhieoh</div>
+    </>
+  )
+}
+
+export default Cake
--- app/Menu/page.js
+++ app/Menu/page.js
@@ -1,22 +1,32 @@
 import Cake from "@/components/cake";
 import React from "react";
-import '@/Styles/menu.css';
-import Data from '@/Data/data.json'
+import "@/Styles/menu.css";
+import Data from "@/Data/data.json";
+import Link from "next/link";
 const Menu = () => {
   // Data.map((t,i)=>{
   //   console.log(t.title);
   // })
   return (
     <>
-    <div className="menu-main">
-      {Data.map((t,i)=>{
-        return(<Cake data={t} key={t.key}/>)
-      })}
-      
-    </div>
-      
+      <div className="links-menu">
+        <span className="links-pages">
+          <Link href="/Menu" className="cake-link st1">
+            Cake
+          </Link>
+          <Link href="https://wa.link/5qgflv" className="cake-link st2" target="blank">
+            CupCake
+          </Link>
+          <span className="link-style"></span>
+        </span>
+      </div>
+      <div className="menu-main">
+        {Data.map((t, i) => {
+          return <Cake data={t} key={t.key} />;
+        })}
+      </div>
     </>
-  )
+  );
 };
 
 export default Menu;

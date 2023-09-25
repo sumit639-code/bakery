0910777363ccf9cc2a4be26987f7cc9504214bbe
Sumit639 <sumitkkumar639@gmail.com>
2023-09-26 00:19:29 +0530

    some css in Dynamic route pages

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,24 +1,60 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import Data from "@/Data/data.json";
-import '@/Styles/dyn-route.css'
-const page = ({params}) => {
-    const id = params.id;
-    const dyndata = Data[id]
+import "@/Styles/dyn-route.css";
+import Image from "next/image";
+
+const page = ({ params }) => {
+  const [count, setcount] = useState(1);
+  const id = params.id;
+  const dyndata = Data[id];
   return (
     <>
-    <div className="main-dyn">
-      <h1 className="title-dyn">
-        {dyndata.title}
-      </h1>
-      <span className="desc-dyn">
-        {dyndata.desc}
-      </span>
-      <span className="price-dyn">
-        {dyndata.price}
-      </span>
-    </div>
+      <div className="main-dyn">
+        <Image
+          src={dyndata.img}
+          width={500}
+          height={500}
+          loading="lazy"
+          alt="Image of cake"
+          decoding="async"
+          fetchPriority="high"
+          className="img-dyn"
+        />
+        <div className="text-dyn">
+          <h1 className="title-dyn">{dyndata.title}</h1>
+          <span className="desc-dyn">{dyndata.desc}</span>
+          <span className="price-dyn">₹ {dyndata.price}</span>
+          <div className="cnt">
+            <button
+              onClick={() => {
+                if (count <= 0) {
+                  setcount(0);
+                } else {
+                  setcount(count - 1);
+                }
+              }}
+            >
+              -
+            </button>
+            <span className="cnt-text">{count}</span>
+            <button
+              onClick={() => {
+                if (count >= 9) {
+                  setcount(count);
+                } else {
+                  setcount(count + 1);
+                }
+              }}
+            >
+              +
+            </button>
+          </div>
+          <button className="buy-dyn">Buy now</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;

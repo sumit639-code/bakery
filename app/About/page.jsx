70dda70b3586174e09455e9ba83594f6c221f14a
Sumit639 <sumitkkumar639@gmail.com>
2023-10-09 23:31:13 +0530

    added the about page and some more animation

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,22 +1,177 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-import '@/Styles/about.css'
+import "@/Styles/about.css";
+import { motion, useScroll, useSpring } from "framer-motion";
+
 const page = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <>
-      <div className="mainabt">
-        <div className="titlemain">
-          <div className="tit">About Lilly's Bakery</div>
+      <motion.div className="progress-bar" style={{ scaleX: scaleX }} />
 
-          <div className="descabt">
-            Welcome to Lilly's Bakery, where our passion for baking meets your
-            love for delightful treats. Established in 2020, we have been
-            serving our community with the finest, handcrafted baked goods for 3
-            years
-          </div>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{
+          duration: 0.6,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay:0.4,
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        viewport={{ once: true }}
+      >
+        <div className="mainabt">
+          
+          <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{
+          duration: 0.6,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay:0.05,
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        viewport={{ once: true }}
+      ><div className="titlemain">
+            <div className="tit">About Lilly's Bakery</div>
+
+            <div className="descabt">
+              Welcome to Lilly's Bakery, where our passion for baking meets your
+              love for delightful treats. Established in 2020, we have been
+              serving our community with the finest, handcrafted baked goods for
+              3 years
+            </div>
+          </div></motion.div>
+          <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{
+          duration:1,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay:0.4,
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        viewport={{ once: true }}
+      ><div className="titlemain">
+            <div className="tit">Our Story</div>
+
+            <div className="descabt">
+              It all began with a dream and a pinch of flour. [Your Name], the
+              founder of [Your Bakery's Name], started this journey with a
+              simple desire to share the joy of freshly baked goods with the
+              world. With a lifelong passion for baking and a dedication to
+              quality, [Your Name] set out to create a bakery that would become
+              a cornerstone of our community. Over the years, we've grown from a
+              small, cozy kitchen to a beloved neighborhood bakery, but our
+              commitment to excellence and the art of baking has never wavered.
+              Every day, our skilled bakers pour their hearts into crafting each
+              pastry, bread, and dessert, ensuring that every bite is a taste of
+              perfection.
+            </div>
+          </div></motion.div>
+          <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{
+          duration: 1,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay:0.4,
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        viewport={{ once: true }}
+      ><div className="titlemain">
+            <div className="tit">Our Philosophy</div>
+
+            <div className="descabt">
+              At [Your Bakery's Name], we believe that baking is not just a
+              craft; it's an art form. We meticulously source the finest
+              ingredients, from the creamiest butter to the richest chocolate,
+              to create our mouthwatering creations. We pride ourselves on our
+              dedication to using locally sourced, organic ingredients whenever
+              possible, supporting both our local farmers and our commitment to
+              sustainability. Our team of bakers is as diverse as our selection
+              of pastries. With years of experience and a shared love for
+              baking, they bring their unique talents and creativity to the
+              kitchen, resulting in a menu that boasts both traditional
+              favorites and innovative new flavors.
+            </div>
+          </div></motion.div>
+          <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{
+          duration: 1,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay:0.4,
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        viewport={{ once: true }}
+      ><div className="titlemain">
+            <div className="tit">Visit Us</div>
+
+            <div className="descabt">
+              We invite you to visit our bakery and experience the warmth and
+              aroma that fill the air. Whether you're looking for a sweet
+              indulgence, a fresh loaf of bread, or a special cake for a
+              celebration, we have something to satisfy your every craving.
+              Thank you for being a part of our journey. We look forward to
+              serving you and creating sweet memories for many years to come.
+              [Contact Information and Location] [Social Media Links]
+            </div>
+          </div></motion.div>
         </div>
-
-      </div>
+      </motion.div>
     </>
   );
 };
--- components/cake.jsx
+++ components/cake.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { CartState } from "@/app/state/atoms/CartState";
+import { motion } from "framer-motion";
 // "use server"
 import Image from "next/image";
 import Link from "next/link";
@@ -10,18 +11,18 @@ import { useRecoilState } from "recoil";
 // const getlocalstorage = JSON.parse(localStorage.getItem('Cake'));
 
 const cake = (props) => {
-  const [cart,setCart]= useRecoilState(CartState);
+  const [cart, setCart] = useRecoilState(CartState);
   const id = props.data.key;
   // useEffect(()=>{
   //   let data = window.localStorage.getItem('cake');
   //   if (data != null ) setCart(JSON.parse(data))
-    
+
   // },[])
   // useEffect(()=>{
   //   window.localStorage.setItem('Cake',JSON.stringify(cart))
-    
+
   // },[cart])
-  
+
   function notify() {
     toast.success(`${props.data.title} Added to the Cart`, {
       position: "top-center",
@@ -32,51 +33,75 @@ const cake = (props) => {
       draggable: true,
       progress: undefined,
       theme: "dark",
-      });
+    });
     addtoCart();
   }
 
-  function addtoCart(){
-    if(cart.findIndex(pro=>pro.key === props.data.key) === -1){
-      setCart(prevData=> [...prevData,props.data])
+  function addtoCart() {
+    if (cart.findIndex((pro) => pro.key === props.data.key) === -1) {
+      setCart((prevData) => [...prevData, props.data]);
       // console.log(cart.findIndex(key=>console.log(key)))
-    }
-    else{
-      console.log(cart.findIndex(key=>console.log(key.key)))
-      setCart(prevData=>{
-        return prevData.map((Item)=>{
-          return Item.key === props.data.key ? {...Item, quantity:Item.quantity+1}:Item
-        })
-      })
+    } else {
+      console.log(cart.findIndex((key) => console.log(key.key)));
+      setCart((prevData) => {
+        return prevData.map((Item) => {
+          return Item.key === props.data.key
+            ? { ...Item, quantity: Item.quantity + 1 }
+            : Item;
+        });
+      });
     }
   }
-  
- 
-  
+
   return (
     <>
-      <div className="card-main">
-        <Image
-          src={props.data.img}
-          height={500}
-          width={500}
-          loading="lazy"
-          alt="Image of cupx  cake"
-          decoding="async"
-          fetchPriority="high"
-          className="card-img"
-        />
-        <h1 className="card-title">
-          <Link href={`/${props.data.key}`} className="link">
-            {props.data.title}
-          </Link>
-        </h1>
-        <p className="card-desc">{props.data.desc}</p>
-        <h1 className="card-price">₹ {props.data.price}{props.pc}</h1>
-        <button className="card-btn" onClick={notify}>
-          Add to Cart
-        </button>
-      </div>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{
+          duration: 1,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay:Math.random()*0.5,
+          scale: {
+            type: "spring",
+            damping: 6,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        
+        viewport={{ once: true,amount: 0.2 }}
+      >
+        <div className="card-main">
+          <Image
+            src={props.data.img}
+            height={500}
+            width={500}
+            loading="lazy"
+            alt="Image of cupx  cake"
+            decoding="async"
+            fetchPriority="high"
+            className="card-img"
+          />
+          <h1 className="card-title">
+            <Link href={`/${props.data.key}`} className="link">
+              {props.data.title}
+            </Link>
+          </h1>
+          <p className="card-desc">{props.data.desc}</p>
+          <h1 className="card-price">
+            ₹ {props.data.price}
+            {props.pc}
+          </h1>
+          <button className="card-btn" onClick={notify}>
+            Add to Cart
+          </button>
+        </div>
+      </motion.div>
       <ToastContainer
         position="top-center"
         autoClose={1000}
